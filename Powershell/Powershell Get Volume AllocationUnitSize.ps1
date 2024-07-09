51d4669ccb77d216129a90cfb4853c8345af075c
Eitan Blumin <eitan@madeiradata.com>
2024-07-09 08:19:52 +0300

    Create Powershell Get Volume AllocationUnitSize.ps1

---
+++ Powershell/Powershell Get Volume AllocationUnitSize.ps1
@@ -0,0 +1,12 @@
+﻿<#
+.SYNOPSIS
+Get the allocation unit size of every disk volume.
+If you have a SQL Server, ensure the block size is 64 KB for the database and log files.
+
+.LINK
+https://serverfault.com/a/904263
+https://www.alitajran.com/get-allocation-unit-size-powershell/
+#>
+Get-CimInstance -ClassName Win32_Volume | Select-Object Name, FileSystem, Label, BlockSize | Sort-Object Name | Format-Table -AutoSize
+
+Get-Volume | Select-Object DriveLetter, FileSystemLabel, AllocationUnitSize | Sort-Object DriveLetter | Get-Unique -AsString
