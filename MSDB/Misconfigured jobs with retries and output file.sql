8d243e87ffb730d4825ba5f32a9990eecc041f0c
Eitan Blumin <eitan@madeiradata.com>
2025-06-30 17:28:19 +0300

    Added remediation command

--- MSDB/Misconfigured jobs with retries and output file.sql
+++ MSDB/Misconfigured jobs with retries and output file.sql
@@ -1,7 +1,7 @@
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 
 SELECT 
-    CONCAT(N'Job "', j.name, N'" step ', s.step_id, N' ("', s.step_name,N'") has retries enabled but '
+    Msg = CONCAT(N'Job "', j.name, N'" step ', s.step_id, N' ("', s.step_name,N'") has retries enabled but '
     , CASE WHEN (s.flags & 2) = 0 THEN N'Append is disabled (which may cause loss of info) ' ELSE N'Append is enabled ' END
     , CASE WHEN
                 s.output_file_name NOT LIKE '%(TIME)%'
@@ -14,6 +14,27 @@ SELECT
         ELSE N'' END
     , N' for the output file path: ', s.output_file_name)
     , s.step_id
+    , RemediationCommand = N'-- REVIEW BEFORE EXECUTION:
+    EXEC msdb.dbo.sp_update_jobstep 
+        @job_name = N' + QUOTENAME(j.name, '''') + ', 
+        @step_id = ' + CAST(s.step_id AS NVARCHAR(MAX)) + ', 
+        @output_file_name = N''' + 
+        CASE 
+            WHEN     s.output_file_name NOT LIKE '%(TIME)%'
+                 AND s.output_file_name NOT LIKE '%(STRTTM)%'
+                 AND s.output_file_name NOT LIKE '%(STRTDT)%'
+                 AND s.output_file_name NOT LIKE '%(DATE)%'
+            THEN
+                -- Insert tokens before .txt (case-insensitive)
+                CASE 
+                    WHEN s.output_file_name LIKE N'%.txt'
+                    THEN LEFT(s.output_file_name, LEN(s.output_file_name) - 4) + '_$(ESCAPE_SQUOTE(DATE))_$(ESCAPE_SQUOTE(TIME)).txt'
+                    ELSE s.output_file_name + '_$(ESCAPE_SQUOTE(DATE))_$(ESCAPE_SQUOTE(TIME))'
+                END
+            ELSE
+                s.output_file_name
+        END + ''', 
+        @flags = ' + CAST(s.flags | 2 AS NVARCHAR(MAX))
 FROM 
     msdb.dbo.sysjobs AS j
 INNER JOIN 
