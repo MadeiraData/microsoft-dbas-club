2e396b3269f48a1d5c3ea43f5ebbefbecd5edc22
Eitan Blumin <eitan@madeiradata.com>
2025-07-05 09:34:35 +0300

    add support for misconfigured output files without retries

--- MSDB/Misconfigured jobs with retries and output file.sql
+++ MSDB/Misconfigured jobs with retries and output file.sql
@@ -1,7 +1,8 @@
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 
 SELECT 
-    Msg = CONCAT(N'Job "', j.name, N'" step ', s.step_id, N' ("', s.step_name,N'") has retries enabled but '
+    Msg = CONCAT(N'Job "', j.name, N'" step ', s.step_id, N' ("', s.step_name,N'") ',
+    , CARE WHEN s.retry_attempts > 0 THEN N'has retries enabled but ' ELSE N'' END
     , CASE WHEN (s.flags & 2) = 0 THEN N'Append is disabled (which may cause loss of info) ' ELSE N'Append is enabled ' END
     , CASE WHEN
                 s.output_file_name NOT LIKE '%(TIME)%'
@@ -27,7 +28,7 @@ SELECT
             THEN
                 -- Insert tokens before .txt (case-insensitive)
                 CASE 
-                    WHEN s.output_file_name LIKE N'%.txt'
+                    WHEN LOWER(s.output_file_name) LIKE N'%.txt'
                     THEN LEFT(s.output_file_name, LEN(s.output_file_name) - 4) + '_$(ESCAPE_SQUOTE(DATE))_$(ESCAPE_SQUOTE(TIME)).txt'
                     ELSE s.output_file_name + '_$(ESCAPE_SQUOTE(DATE))_$(ESCAPE_SQUOTE(TIME))'
                 END
@@ -41,14 +42,14 @@ INNER JOIN
     msdb.dbo.sysjobsteps AS s ON j.job_id = s.job_id
 WHERE 
     j.[enabled] = 1
-    AND s.retry_attempts > 0
     AND s.output_file_name IS NOT NULL
     AND LTRIM(RTRIM(s.output_file_name)) <> ''
     AND (
-            (s.flags & 2) = 0  -- Append to output file is disabled
+            (s.retry_attempts > 0 AND (s.flags & 2) = 0)  -- Retry is enabled but Append to output file is disabled
             OR
             (
-                s.output_file_name NOT LIKE '%(TIME)%'
+                (s.flags & 2) > 0 -- Append enabled
+            AND s.output_file_name NOT LIKE '%(TIME)%'
             AND s.output_file_name NOT LIKE '%(STRTTM)%'
             AND s.output_file_name NOT LIKE '%(STRTDT)%'
             AND s.output_file_name NOT LIKE '%(DATE)%'
