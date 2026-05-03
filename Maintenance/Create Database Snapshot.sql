03211e1726abe1661885155bafd0c322fd85bc18
Eitan Blumin <eitan@madeiradata.com>
2026-05-03 15:30:28 +0300

    Update Create Database Snapshot.sql

--- Maintenance/Create Database Snapshot.sql
+++ Maintenance/Create Database Snapshot.sql
@@ -1,4 +1,4 @@
-USE MyDB
+USE [master]
 GO
 
 SET ANSI_NULLS ON
@@ -9,16 +9,17 @@ GO
 
 /*******************************************************************************************************************
  Title:		A procedure to create Database Snapshot
- Author:	Reut Almog Talmi @Madeira
+ Author:	Reut Almog Talmi and Eitan Blumin @Madeira
 ********************************************************************************************************************/
 CREATE OR ALTER PROCEDURE [dbo].[sp_DBA_CreateDatabaseSnapshot]
 	@SourceDBName		SYSNAME,
-	@Suffix				NVARCHAR(20) = NULL,
+	@NewSnapshotDBName	SYSNAME = NULL,
+	@FileSuffix			NVARCHAR(20) = NULL,
 	@DestFilePath		NVARCHAR(200) = NULL, -- Edit when you want the destination snapshot file to reside somewhere(Example: 'Z:\Path\')
 	@IgnoreReplicaRole	BIT = 0,			/*By default, the snapshot will be created on the secondary replica only. 
 											in case desired to create on primary - set @IgnoreReplicaRole to 1
 											For databases not involved in AG -  @IgnoreReplicaRole can be 0 or 1 or NULL*/
-	@Debug BIT = 1
+	@Debug BIT = 0
 
 AS
 
@@ -26,9 +27,8 @@ SET NOCOUNT ON
 
 
 DECLARE  	
-	@FilePath NVARCHAR(3000) = '',
-	@SQLCommand NVARCHAR(4000) = '',
-	@SnapshotDatabaseName SYSNAME
+	@FilePath NVARCHAR(3000),
+	@SQLCommand NVARCHAR(4000) = ''
 	
 
 IF DB_ID(@SourceDBName) IS NULL OR NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = @SourceDBName) 
@@ -36,12 +36,13 @@ BEGIN
 	RAISERROR('Database %s doesn''t exist',16,1,@SourceDBName)
 END
 
-
 -- Set default suffix with timestamp if it is not defined
-IF @Suffix IS NULL OR TRIM(@Suffix) = ''
+IF @FileSuffix IS NULL OR TRIM(@FileSuffix) = ''
 BEGIN
-	SELECT @Suffix = REPLACE(REPLACE(REPLACE(CONVERT(VARCHAR(20), GETDATE(), 120), ':', ''), '-','_'), ' ', '_')
+	SELECT @FileSuffix = REPLACE(REPLACE(REPLACE(CONVERT(VARCHAR(20), GETDATE(), 120), ':', ''), '-','_'), ' ', '_')
 END
+
+SET @NewSnapshotDBName = ISNULL(@NewSnapshotDBName, @SourceDBName + '_' + @FileSuffix)
 
 
 -- Set the file path location of the snapshot data files.
@@ -60,19 +61,22 @@ END
 IF ISNULL((SELECT sys.fn_hadr_is_primary_replica (@SourceDBName)), 0) = 0 OR  @IgnoreReplicaRole = 1
 BEGIN
 
-	SELECT @FilePath += N'' + mf.name + ', FILENAME = ''' + ISNULL(@DestFilePath, LEFT(mf.physical_name,LEN(mf.physical_name)- 4 ) ) + '_' + @Suffix + '.ss'')'
+	SELECT @FilePath =
+	ISNULL(@FilePath + N',
+', N'') + N'(NAME = ' + QUOTENAME(mf.name) + N', FILENAME = ''' + ISNULL(@DestFilePath, LEFT(physical_name, LEN(physical_name) - CHARINDEX('\', REVERSE(physical_name)) + 1))
++ '_' + @FileSuffix + '.ss'
++ N''')'
 	FROM sys.master_files AS mf
 	INNER JOIN sys.databases AS db ON db.database_id = mf.database_id
 	WHERE db.state = 0
 	AND mf.type = 0 -- Only include data files
 	AND db.name = @SourceDBName
 
-
-
-
 	SET @SQLCommand += 
-	N'CREATE DATABASE ' + QUOTENAME(@SourceDBName + '_' + @Suffix ) + CHAR(10)+ 
-	' ON ( NAME = ' + @FilePath + ' AS SNAPSHOT OF '+ QUOTENAME(@SourceDBName) + ';' + CHAR(30)
+	N'CREATE DATABASE ' + QUOTENAME(@NewSnapshotDBName) + CHAR(10)
++ ISNULL(N'
+ON ' + @FilePath, N'') 
++ N' AS SNAPSHOT OF '+ QUOTENAME(@SourceDBName) + ';' + CHAR(30)
 
 END
 
@@ -83,13 +87,20 @@ IF @Debug != 1
 BEGIN
 	EXEC sp_executesql @SQLCommand
 	
-	SELECT TOP 1 @SnapshotDatabaseName = name 
-	FROM sys.databases 
-	WHERE  source_database_id = DB_ID(@SourceDBName) 
-	ORDER BY create_date DESC
-
-	RAISERROR ('Database [%s] has been created',1,1,@SnapshotDatabaseName)
+	IF DB_ID(@NewSnapshotDBName) IS NOT NULL
+		RAISERROR ('Database snapshot [%s] has been created',1,1,@NewSnapshotDBName)
+	ELSE
+		RAISERROR ('Error trying to create database snapshot [%s]',1,1,@NewSnapshotDBName)
 END
 GO
 
 
+IF OBJECT_ID('sp_MS_marksystemobject', 'P') IS NOT NULL AND DB_NAME() = 'master'
+BEGIN
+	PRINT 'Adding system object flag to allow procedure to be used within all databases'
+	EXEC sp_MS_marksystemobject 'sp_DBA_CreateDatabaseSnapshot'
+END
+PRINT 'Granting EXECUTE permission on stored procedure to all users'
+GRANT EXEC ON [sp_DBA_CreateDatabaseSnapshot] TO [public]
+
+GO
